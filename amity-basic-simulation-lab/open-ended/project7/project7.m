a86889c8eff22761c571731b906c05b4976d3dde
legitShivam <shiv.05cs@gmail.com>
2023-04-27 16:01:23 +0530

    feat: filter salt and pepper noise - filter salt and pepper noise - show filtered image - change the subplot dimensions from 2x2 to 3x3

--- amity-basic-simulation-lab/open-ended/project7/project7.m
+++ amity-basic-simulation-lab/open-ended/project7/project7.m
@@ -2,28 +2,37 @@
 img = imread('circuit.tif');
 
 % Display the original image
-subplot(2, 2, 1);
+subplot(3, 2, 1);
 imshow(img);
 title('Original Image');
 
 % Add Gaussian noise to the image
-noisy_img_gaussian = imnoise(img, 'gaussian', 0.05);
+noisy_img_gaussian = imnoise(img, 'gaussian');
 % Display the noisy image with Gaussian noise
-subplot(2, 2, 2);
+subplot(3, 2, 2);
 imshow(noisy_img_gaussian);
 title('Noisy Image (Gaussian)');
 
 % Add salt and pepper noise to the image
-noisy_img_sp = imnoise(img, 'salt & pepper', 0.05);
+noisy_img_sp = imnoise(img, 'salt & pepper');
 % Display the noisy image with salt and pepper noise
-subplot(2, 2, 3);
+subplot(3, 2, 3);
 imshow(noisy_img_sp);
 title('Noisy Image (Salt and Pepper)');
 
 % Apply Gaussian filter to the noisy image
-filtered_img_gaussian = imgaussfilt(noisy_img_gaussian, 2);
+filtered_img_gaussian = imgaussfilt(noisy_img_gaussian);
+
+% Apply salt and pepper filter to the noisy image
+filtered_img_sp = medfilt2(noisy_img_sp);
+
 % Display the filtered image with Gaussian filter
-subplot(2, 2, 4);
+subplot(3, 2, 4);
 imshow(filtered_img_gaussian);
 title('Filtered Image (Gaussian)');
+
+% Display the filtered image with salt and pepper filter
+subplot(3, 2, 5);
+imshow(filtered_img_sp);
+title('Filtered Image (Salt and Pepper)');
 
